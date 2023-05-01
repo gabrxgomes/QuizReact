b615ac1611429bdc4d8478b4ec793da578dc1786
Gabriel <gabrielnasciment123@hotmail.com>
2023-05-01 16:32:52 -0300

    adicionando alterações 010523

--- src/App.js
+++ src/App.js
@@ -28,18 +28,76 @@ function App() {
 
   //console.log(words); TESTTE PARA OBSERVAR O OBJETO SENDO LANÇADO NO CONSOLE DO NAVEGADOR
 
+  const [pickedWord, setPickedWord] = useState("")
+  const [pickedCategory, setPickedCategory] = useState("")
+  const [letters, setLetters] = useState([]);
+
+
+  const [guessedLetters, setGuessedLetters] = useState([])
+  const [wrongLetters, setWrongLetters] = useState([])
+  const [guesses, setGuesses] = useState(3)
+  const [score, setScore] = useState(0)
+
+  const pickWordAndCategory = () => {
+    //pegar uma categoria aleatoria baseada no tamanho da lista das categorias (total)
+    const categories = Object.keys(words)
+    const category = 
+      categories[Math.floor(Math.random() * Object.keys(categories).length)];
+
+    console.log(category);//apos executada a função que randomiza uma categoria
+
+    //pegar uma palavra
+    const word = 
+      words[category][Math.floor(Math.random() * words[category].length)];
+
+    return { word, category }
+
+  };
 
 
   //startando o game 
   const startGame = () => {
+    //vamo criar uma func para escolher a palavra e selecionar a categoria dentro do 
+    //tamanho da minha lista de categorias, temos o array [categorias] e o array 
+    //categorias = [palavras]
+    //mudar a pagina corretamente
+
+    const { word, category } = pickWordAndCategory();
+
+
+
+    //criando um array de letras para transformar nossa palavra aleatoria que vem de sua categoria
+
+    let wordLetters = word.split("")
+
+
+
+
+    //o js é key sensitive entao temos que normalizar a primeira letra pois a mesma vem com capslock
+
+
+    wordLetters = wordLetters.map((l) => l.toLowerCase());
+
+    console.log(word, category);
+    console.log(wordLetters);
+
+    // setando os status !
+
+    setPickedWord(word);
+    setPickedCategory(category);
+    setLetters(wordLetters);
+
+    //pickWordAndCategory(); //printa a categoria escolhida baseada no length
+
+    
     setGameStage(stages[1].name);
     console.log("o componente foi chamado!"); //test para saber se a minha funçãa foi chamada
   }
 
 
   //criar função pra verificar o input do usuario
-  const verifyLetter = () => {
-    setGameStage(stages[2]. name);
+  const verifyLetter = (letter) => {
+    console.log(letter);
   }
 
   //reestartar o jogo
@@ -52,7 +110,17 @@ function App() {
   return (
     <div className="App">
       {gameStage === "start" && <StartScreen startGame={startGame} />}
-      {gameStage === "game" && <Game  verifyLetter={verifyLetter}/>}
+      {gameStage === "game" && (<Game  
+      verifyLetter={verifyLetter} 
+      pickedWord={pickedWord}
+      pickedCategory={ pickedCategory }
+      letters={letters}
+      guessedLetters={guessedLetters}
+      wrongLetters={wrongLetters}
+      guesses={guesses}
+      score={score}
+      />
+      )}
       {gameStage === "end" && <GameOver retry={retry}/>}
     </div>
   );
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,14 +1,76 @@
 import "./Game.css";
+import {useState, useRef} from "react";
+const Game = ({
+    verifyLetter, 
+    pickedWord,
+    pickedCategory,
+    letters,
+    guessedLetters,
+    wrongLetters,
+    guesses,
+    score,
+}) => {
+    const [letter, setLetter] = useState("");
+    const letterInputRef = useRef(null)
 
-const Game = ({verifyLetter}) => {
-    return <div>
-            <h1>
-                Game
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+
+        verifyLetter(letter);
+
+        setLetter("");//apaga a letra apos clicar pra iniciar o game
+
+        letterInputRef.current.focus(); //ele foca no input da letra limpando o input e lendo mais rapido as entradas
+    };
+
+
+    return (
+      <div className="game">
+            <p className = "points"> {/* um paragrafo para ser uma box que recebe os pontos */}
+                <span>Pontuação: {score}</span>
+            </p>
+            <h1>Advinhe a palavra:</h1>
+            <h3 className="tip"> 
+                Dica sobre a palavra: <span>{pickedCategory}</span>
+            </h3>
+            <p>Você ainda tem {guesses} tentativas</p>
+            <div className="wordContainer">
+                {letters.map((letter, i) => 
+                    guessedLetters.includes(letter) ? (
+                        <span key = {i} className="letter">{letter}</span>
+                    ) : (
+                        <span key = {i} className="blankSquare"></span>
+
+                    )
+                )}
             
-            </h1>
-            <button onClick={verifyLetter}>Finalizar Jogo</button>
+            </div>  
+            <div className="letterContainer">
+                <p>Tente advinhar uma letra da palavra:</p>
+                <form onSubmit={handleSubmit}>
+                    <input 
+                    type="text" 
+                    name="letter" 
+                    maxLength="1" 
+                    required
+                    onChange={(e) => setLetter(e.target.value)}
+                    value={letter}
+                    ref={letterInputRef} //voce puxa os atributos desse elemento como referencia
+                    />
+                    <button>Jogar!</button>
+                </form>
+            </div>
+            <div className="wrongLettersContainer">
+                <p>Letras já uitlizadas:</p>
+                {wrongLetters.map((letter, i) => (
+                    <span key={1}>{letter}, </span>
+                ))}
+            </div>
+      </div>
         
-        </div>
+    );
+      
 };
 
 export default Game;
